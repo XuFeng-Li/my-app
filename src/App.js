ef0d850409fca4f75d39d7b6f2155a8131b9fed7
李旭峰 <lxf@asman.com.cn>
2020-12-10 11:39:07 +0800

    创建 .env 文件，用于解决 包引用冲突的问题。是否有其他的解决方案？

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from './../src/stories/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+
+                <Button
+                    backgroundColor={'#ABCD09'}
+                    primary={false}
+                    label={'Button'}
+                    size={'small'}
+                    onClick={() => {
+                        alert('123');
+                    }}
+                />
+            </header>
+        </div>
+    );
 }
 
 export default App;
